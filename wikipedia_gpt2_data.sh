ff22cca3098d6cd235a7f519d4d223570c93976a
ghkayne <97163060+ghkayne@users.noreply.github.com>
2022-01-06 10:13:13 +0900

    Update wikipedia_gpt2_data.sh
    Simplify the she script code

--- wikipedia_gpt2_data.sh
+++ wikipedia_gpt2_data.sh
@@ -1,17 +1,8 @@
-python wikipedia_datasets.py --mode 1 --tenth_digit 0 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 1 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 2 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 3 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 4 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 5 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 6 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 7 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 8 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 9 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 10 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 11 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 12 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 13 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 14 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 15 --month 20210801 &
-python wikipedia_datasets.py --mode 1 --tenth_digit 16 --month 20210801 
+#!/bin/bash
+
+for ((i=0;i<17;i++))
+do
+    python wikipedia_datasets.py --mode subset --tenth_digit $i --month 20210801 &
+done
+
+#python wikipedia_datasets.py --mode 1 --tenth_digit 16 --month 20210801 
