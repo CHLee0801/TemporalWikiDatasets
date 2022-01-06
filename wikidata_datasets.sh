dfd2d60316528fa41560ff7048f5acff64c29367
ghkayne <97163060+ghkayne@users.noreply.github.com>
2022-01-06 10:11:07 +0900

    Update wikidata_datasets.sh
    Simplify the shell script code

--- wikidata_datasets.sh
+++ wikidata_datasets.sh
@@ -1,22 +1,8 @@
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 0 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 1 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 2 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 3 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 4 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 5 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 6 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 7 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 8 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 9 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 10 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 11 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 12 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 13 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 14 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 15 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 16 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 17 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 18 --combine 0 &
-python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx 19 --combine 0 
+#!/bin/bash
+
+for ((i=0;i<20;i++))
+do
+    python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx $i --combine 0 &
+done
 
 #python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx -1 --combine 1 
