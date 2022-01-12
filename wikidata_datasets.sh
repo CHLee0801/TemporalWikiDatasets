85b30c8761f6ccbbfaed6256f63b36e36bb59afb
ghkayne <97163060+ghkayne@users.noreply.github.com>
2022-01-12 20:15:19 +0900

    Delete and move to scripts folder
    Delete and move to scripts folder

--- wikidata_datasets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for ((i=0;i<20;i++))
-do
-    python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx $i --combine 0 &
-done
-
-#python wikidata_datasets.py --mode unchanged --old 20210801 --new 20210901 --idx -1 --combine 1 
